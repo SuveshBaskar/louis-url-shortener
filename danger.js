fccbcad5121eb9626ae49d4755c0611951d02220
SuveshBaskar <suvesh@yellowmessenger.com>
2020-08-04 01:12:41 +0530

    ci: added Danger JS

---
+++ danger.js
@@ -0,0 +1,9 @@
+const {fail, warn, message, markdown, danger} = require("danger");
+
+fail("This is a failure message")
+warn("This is a warning")
+message("This is a normal message")
+markdown("*Markdown* is also **supported**")
+
+const { additions = 0, deletions = 0 } = danger.github.pr
+message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
